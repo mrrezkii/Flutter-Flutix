de6045683e4cb01fe485828a55b332822446efc1
nando <muhammad.rezki.ananda@gmail.com>
2021-04-11 06:20:17 +0700

    Wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:bwa_flutix/services/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'ui/pages/pages.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,46 +14,13 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return StreamProvider.value(
+      value: AuthServices.userStream,
+      initialData: null,
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                RaisedButton(
-                    child: Text("Sign Up"),
-                    onPressed: () async {
-                      SignInSignUpResult result = await AuthServices.signUp(
-                          "rezki@mail.com",
-                          "123455622222",
-                          "Nando",
-                          ["Action", "Horrror", "Music", "Drama"],
-                          "Jepang");
-
-                      if (result.userModel == null) {
-                        print(result.message);
-                      } else {
-                        print(result.userModel.toString());
-                      }
-                    }),
-                RaisedButton(
-                    child: Text("Sign In"),
-                    onPressed: () async {
-                      SignInSignUpResult result = await AuthServices.signIn(
-                        "rezki@mail.com",
-                        "123455622222b",
-                      );
-
-                      if (result.userModel == null) {
-                        print(result.message);
-                      } else {
-                        print(result.userModel.toString());
-                      }
-                    })
-              ],
-            ),
-          ),
-        ));
+        home: Wrapper(),
+      ),
+    );
   }
 }
---
+++ lib/ui/pages/wrapper.dart
@@ -0,0 +1,15 @@
+part of 'pages.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    User firebaseUser = Provider.of<User>(context);
+
+    if (firebaseUser == null) {
+      return SignInPage();
+    } else {
+      return MainPage();
+    }
+    return Container();
+  }
+}
