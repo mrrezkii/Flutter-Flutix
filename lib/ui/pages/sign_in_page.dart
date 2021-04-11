41eac6d74b733a96da9ae498c8b10013bf29b989
nando <muhammad.rezki.ananda@gmail.com>
2021-04-11 10:41:37 +0700

    Login Page (1)

--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -6,18 +6,104 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool isEmailValid = false;
+  bool isPasswordValid = false;
+  bool isSigningIn = false;
+
   @override
   Widget build(BuildContext context) {
+    context
+        .read<ThemeBloc>()
+        .add(ChangeTheme(ThemeData().copyWith(primaryColor: accentColor2)));
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sign In Page"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text("Sign In"),
-          onPressed: () {
-            AuthServices.signIn("rezki@mail.com", "123455622222");
-          },
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: 30,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 70,
+                  child: Image.asset("assets/logo.png"),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 70, bottom: 40),
+                  child: Text(
+                    "Welcome Back,\nExplorer!",
+                    style: blackTextFont.copyWith(
+                        fontSize: 28, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      labelText: "Email Address",
+                      hintText: "Email Address"),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      labelText: "Password",
+                      hintText: "Password"),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "Forgot Password",
+                      style: greyTextFont.copyWith(
+                          fontSize: 12, fontWeight: FontWeight.w400),
+                    ),
+                    Text(
+                      "Get Now",
+                      style: purpleTextFont.copyWith(
+                          fontSize: 12, fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+                Center(
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    margin: EdgeInsets.only(top: 40, bottom: 30),
+                    child: FloatingActionButton(
+                      child: Icon(Icons.arrow_forward),
+                      backgroundColor: mainColor,
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Text("Start Fresh Now?",
+                        style:
+                            greyTextFont.copyWith(fontWeight: FontWeight.w400)),
+                    Text("Sign Up",
+                        style: purpleTextFont.copyWith(
+                            fontWeight: FontWeight.w400))
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
