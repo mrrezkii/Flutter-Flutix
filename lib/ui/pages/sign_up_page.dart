db095732a4a69fd1e9cde7260e6483bfea5bc911
nando <muhammad.rezki.ananda@gmail.com>
2021-04-13 13:18:18 +0700

    Sign Up Page (2)

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -23,6 +23,9 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
+    context
+        .read<ThemeBloc>()
+        .add(ChangeTheme(ThemeData().copyWith(primaryColor: accentColor1)));
     return WillPopScope(
       onWillPop: () async {
         context.read<PageBloc>().add(GoToSplashPage());
@@ -51,9 +54,107 @@ class _SignUpPageState extends State<SignUpPage> {
                               child:
                                   Icon(Icons.arrow_back, color: Colors.black),
                             ),
-                          )
+                          ),
+                          Center(
+                            child: Text("Create New\nAccount",
+                                style: blackTextFont.copyWith(fontSize: 20),
+                                textAlign: TextAlign.center),
+                          ),
                         ],
-                      ))
+                      )),
+                  Container(
+                    width: 90,
+                    height: 104,
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          height: 90,
+                          width: 90,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                image: (widget.registrationData.profileImage ==
+                                        null)
+                                    ? AssetImage("assets/user_pic.png")
+                                    : FileImage(
+                                        widget.registrationData.profileImage),
+                              )),
+                        ),
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                                height: 28,
+                                width: 28,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                      image: AssetImage((widget.registrationData
+                                                  .profileImage ==
+                                              null)
+                                          ? "assets/btn_add_photo.png"
+                                          : "assets/btn_del_photo.png")),
+                                )),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 36,
+                  ),
+                  TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        labelText: "Full Name",
+                        hintText: "Full Name"),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        labelText: "Email",
+                        hintText: "Email"),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        labelText: "Password",
+                        hintText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    controller: retypeController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        labelText: "Confirm Password",
+                        hintText: "Confirm Password"),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  FloatingActionButton(
+                      onPressed: () {},
+                      child: Icon(Icons.arrow_forward),
+                      backgroundColor: mainColor,
+                      elevation: 0),
                 ],
               )
             ],
