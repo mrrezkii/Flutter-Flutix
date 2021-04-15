7bf346dee1f4fe6d0dd1e3a0c68b0fdcba247126
nando <muhammad.rezki.ananda@gmail.com>
2021-04-15 18:36:56 +0700

    Movie Page & PromoCard Widget (2)

--- lib/ui/widgets/promo_card.dart
+++ lib/ui/widgets/promo_card.dart
@@ -49,6 +49,73 @@ class PromoCard extends StatelessWidget {
             ],
           ),
         ),
+        ShaderMask(
+          shaderCallback: (rectangle) {
+            return LinearGradient(
+                    begin: Alignment.centerRight,
+                    end: Alignment.centerLeft,
+                    colors: [Colors.black.withOpacity(0.1), Colors.transparent])
+                .createShader(Rect.fromLTRB(0, 0, 77.5, 80));
+          },
+          blendMode: BlendMode.dstIn,
+          child: SizedBox(
+            height: 80,
+            width: 77.5,
+            child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    bottomLeft: Radius.circular(15)),
+                child: Image.asset("assets/reflection2.png")),
+          ),
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: ShaderMask(
+            shaderCallback: (rectangle) {
+              return LinearGradient(
+                  end: Alignment.centerRight,
+                  begin: Alignment.centerLeft,
+                  colors: [
+                    Colors.black.withOpacity(0.1),
+                    Colors.transparent
+                  ]).createShader(Rect.fromLTRB(0, 0, 96, 45));
+            },
+            blendMode: BlendMode.dstIn,
+            child: SizedBox(
+              height: 45,
+              width: 96,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.asset("assets/reflection1.png")),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: ShaderMask(
+            shaderCallback: (rectangle) {
+              return LinearGradient(
+                  end: Alignment.centerRight,
+                  begin: Alignment.centerLeft,
+                  colors: [
+                    Colors.black.withOpacity(0.1),
+                    Colors.transparent
+                  ]).createShader(Rect.fromLTRB(0, 0, 53, 25));
+            },
+            blendMode: BlendMode.dstIn,
+            child: SizedBox(
+              height: 25,
+              width: 53,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.asset("assets/reflection1.png")),
+            ),
+          ),
+        ),
       ],
     );
   }
